65fb5949289fa3ff311e6305230970495c2d0e7d
Fatma Haddad <haddaadfatma@gmail.com>
2019-02-14 12:51:30 +0100

    added `addTopic`, `editTopic`, `deleteTopic`, `show topics` and `show one topic` actions and added DateTimeNormalizer service

--- src/Controller/TopicsController.php
+++ src/Controller/TopicsController.php
@@ -13,8 +13,121 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use App\Entity\Topics;
+use App\Entity\Categories;
 
 class TopicsController extends AbstractController
 {
+    /**
+     * add new topic
+     * @param Request $request
+     * @return View
+     * @throws \Exception
+     */
+    public function addTopic(Request $request)
+    {
+        $topic = new Topics();
+        $subject = $request->get('subject');
+        $date = new \DateTime();
+        $category = $this->getDoctrine()->getRepository('App\Entity\Categories')->find($request->get('category_id'));
+        $user = $this->getUser();
+        if(empty($subject) || empty($category))
+        {
+            return View::create("NULL VALUES ARE NOT ALLOWED", Response::HTTP_NOT_ACCEPTABLE);
+        }
+        else {
+            $topic->setSubject($subject);
+            $topic->setCategory($category);
+            $topic->setDate($date);
+            $topic->setUser($user);
 
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($topic);
+            $em->flush();
+
+            return View::create($topic, Response::HTTP_CREATED, []);
+        }
+    }
+
+    /**
+     * edit topic
+     * @param Request $request
+     * @param $id
+     * @return View
+     * @throws \Exception
+     */
+    public function editTopic(Request $request, $id)
+    {
+        $topic = new Topics();
+
+        $subject = $request->get('subject');
+        $date = new \DateTime();
+        $category = $this->getDoctrine()->getRepository('App\Entity\Categories')->find($request->get('category_id'));
+        $user = $this->getUser();
+
+        $em = $this->getDoctrine()->getManager();
+        $topic = $this->getDoctrine()->getRepository('App\Entity\Topics')->find($id);
+        if (empty($topic)) {
+            return new View("Topic not found", Response::HTTP_NOT_FOUND);
+        }
+        elseif(empty($subject) || empty($category))
+        {
+            return View::create("NULL VALUES ARE NOT ALLOWED", Response::HTTP_NOT_ACCEPTABLE);
+        }
+        else
+        {
+            $topic->setSubject($subject);
+            $topic->setCategory($category);
+            $topic->setDate($date);
+            $topic->setUser($user);
+            $em->flush();
+            return View::create($topic, Response::HTTP_CREATED, []);
+        }
+    }
+
+    /**
+     * delete topic
+     * @param $id
+     * @return View
+     */
+    public function deleteTopic($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $topic = $em->getRepository('App\Entity\Topics')->find($id);
+        if (empty($topic)) {
+            return new View("Topic not found", Response::HTTP_NOT_FOUND);
+        }
+        else
+        {
+            $em->remove($topic);
+            $em->flush();
+            return View::create("Topic Deleted Successfully", Response::HTTP_OK);
+        }
+    }
+
+    /**
+     * show all topics
+     * @return View
+     */
+    public function topics()
+    {
+        $topics = $this->getDoctrine()->getRepository('App\Entity\Topics')->findAll();
+        if (empty($topics)) {
+            return new View("No topics found", Response::HTTP_NOT_FOUND);
+        }
+        return View::create($topics, Response::HTTP_OK, []);
+    }
+
+    /**
+     * show one topic
+     * @param $id
+     * @return View
+     */
+    public function topic($id)
+    {
+        $topic = $this->getDoctrine()->getRepository('App\Entity\Topics')->find($id);
+        if (empty($topic)) {
+            return new View("Topic can not be found", Response::HTTP_NOT_FOUND);
+        }
+        return View::create($topic, Response::HTTP_OK, []);
+    }
 }
