20e702a693bf8ce5cf3c414807fa77f67c178ccf
Fatma Haddad <haddaadfatma@gmail.com>
2019-02-17 12:07:12 +0100

    fixed `use .. ArrayCollection and Collection` missing in `Replies` entity

--- src/Entity/Replies.php
+++ src/Entity/Replies.php
@@ -8,7 +8,8 @@
 
 namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
-
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 /**
  * @ORM\Table(name="replies")
  * @ORM\Entity
