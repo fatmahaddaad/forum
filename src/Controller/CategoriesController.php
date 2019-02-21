43027858541795496969e7199bd601037b2d4193
Fatma Haddad <haddaadfatma@gmail.com>
2019-02-21 13:19:58 +0100

    added api documentation in `CategoriesController`

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -13,6 +13,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use App\Entity\Categories;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Nelmio\ApiDocBundle\Annotation\Security;
+use Swagger\Annotations as SWG;
+use Symfony\Component\Routing\Annotation\Route;
 
 class CategoriesController extends AbstractController
 {
@@ -20,6 +24,33 @@ class CategoriesController extends AbstractController
      * add new category
      * @param Request $request
      * @return View
+     *
+     * @Route("api/addCategory", methods={"POST"})
+     *
+     * @SWG\Tag(name="Category")
+     * @SWG\Response(
+     *     response=201,
+     *     description="Returns created category"
+     * )
+     * @SWG\Response(
+     *     response=403,
+     *     description="Returns access denied if not admin or moderator"
+     * )
+     * @SWG\Response(
+     *     response=406,
+     *     description="Returned when Null Values given"
+     * )
+     *
+     * @SWG\Parameter(
+     *     name="Values",
+     *     in="body",
+     *     description="Category Info",
+     *     required=true,
+     *     @SWG\Schema(
+     *          @SWG\Property(property="name", type="string"),
+     *          @SWG\Property(property="description", type="string")
+     *     )
+     * )
      */
     public function addCategory(Request $request)
     {
@@ -28,7 +59,7 @@ class CategoriesController extends AbstractController
         $description = $request->get('description');
         if(empty($name) || empty($description))
         {
-            return View::create("NULL VALUES ARE NOT ALLOWED", Response::HTTP_NOT_ACCEPTABLE);
+            return View::create(array("code" => 406, "message" => "NULL VALUES ARE NOT ALLOWED"), Response::HTTP_NOT_ACCEPTABLE);
         }
         else {
             $category->setName($name);
@@ -47,6 +78,39 @@ class CategoriesController extends AbstractController
      * @param Request $request
      * @param $id
      * @return View
+     *
+     * @Route("api/editCategory/{id}", methods={"PUT"})
+     *
+     * @SWG\Tag(name="Category")
+     * @SWG\Response(
+     *     response=200,
+     *     description="Returns category"
+     * )
+     * @SWG\Response(
+     *     response=403,
+     *     description="Returns access denied if not admin or moderator"
+     * )
+     * @SWG\Response(
+     *     response=406,
+     *     description="Returned when Null Values given"
+     * )
+     * @SWG\Response(
+     *     response=404,
+     *     description="Returned when category not found"
+     * )
+     *
+     * @SWG\Parameter(
+     *     name="Values",
+     *     in="body",
+     *     description="Category Info",
+     *     required=true,
+     *     @SWG\Schema(
+     *          @SWG\Property(property="name", type="string"),
+     *          @SWG\Property(property="description", type="string")
+     *     )
+     * )
+     *
+     *
      */
     public function editCategory(Request $request, $id)
     {
@@ -56,18 +120,18 @@ class CategoriesController extends AbstractController
         $em = $this->getDoctrine()->getManager();
         $category = $this->getDoctrine()->getRepository('App\Entity\Categories')->find($id);
         if (empty($category)) {
-            return new View("Tutorial not found", Response::HTTP_NOT_FOUND);
+            return new View(array("code" => 404, "message" => "Category not found"), Response::HTTP_NOT_FOUND);
         }
         elseif(empty($name) || empty($description))
         {
-            return View::create("NULL VALUES ARE NOT ALLOWED", Response::HTTP_NOT_ACCEPTABLE);
+            return View::create(array("code" => 404, "message" => "NULL VALUES ARE NOT ALLOWED"), Response::HTTP_NOT_ACCEPTABLE);
         }
         else
         {
             $category->setName($name);
             $category->setDescription($description);
             $em->flush();
-            return View::create($category, Response::HTTP_CREATED, []);
+            return View::create($category, Response::HTTP_OK, []);
         }
     }
 
@@ -75,56 +139,116 @@ class CategoriesController extends AbstractController
      * delete category
      * @param $id
      * @return View
+     *
+     * @Route("api/deleteCategory/{id}", methods={"POST"})
+     *
+     * @SWG\Tag(name="Category")
+     * @SWG\Response(
+     *     response=200,
+     *     description="Returns success message"
+     * )
+     * @SWG\Response(
+     *     response=403,
+     *     description="Returns access denied if not admin or moderator"
+     * )
+     * @SWG\Response(
+     *     response=404,
+     *     description="Returned when category not found"
+     * )
+     *
+     *
      */
     public function deleteCategory($id)
     {
         $em = $this->getDoctrine()->getManager();
         $category = $em->getRepository('App\Entity\Categories')->find($id);
         if (empty($category)) {
-            return new View("Category not found", Response::HTTP_NOT_FOUND);
+            return new View(array("code"=> 404, "message" => "Category not found"), Response::HTTP_NOT_FOUND);
         }
         else
         {
             $em->remove($category);
             $em->flush();
-            return View::create("Category Deleted Successfully", Response::HTTP_OK);
+            return View::create(array("code"=> 200, "message" => "Category Deleted Successfully"), Response::HTTP_OK);
         }
     }
 
     /**
      * show all categories
      * @return View
+     *
+     * @Route("api/categories", methods={"GET"})
+     *
+     * @SWG\Tag(name="Category")
+     * @SWG\Response(
+     *     response=200,
+     *     description="Returns list of categories"
+     * )
+     * @SWG\Response(
+     *     response=404,
+     *     description="Returned when no categories found"
+     * )
+     *
      */
     public function categories()
     {
         $categories = $this->getDoctrine()->getRepository('App\Entity\Categories')->findAll();
         if (empty($categories)) {
-            return new View("No categories found", Response::HTTP_NOT_FOUND);
+            return new View(array("code" => 404 ,"message" => "No categories found"), Response::HTTP_NOT_FOUND);
         }
         return View::create($categories, Response::HTTP_OK, []);
     }
 
     /**
-     * show one category
+     * show one category by ID
      * @param $id
      * @return View
+     *
+     * @Route("api/category/{id}", methods={"GET"})
+     *
+     * @SWG\Tag(name="Category")
+     * @SWG\Response(
+     *     response=200,
+     *     description="Returns list of categories"
+     * )
+     * @SWG\Response(
+     *     response=404,
+     *     description="Returned when category no found"
+     * )
      */
     public function category($id)
     {
         $category = $this->getDoctrine()->getRepository('App\Entity\Categories')->find($id);
         if (empty($category)) {
-            return new View("Category can not be found", Response::HTTP_NOT_FOUND);
+            return new View(array("code" => 404 ,"message" => "Category can not be found"), Response::HTTP_NOT_FOUND);
         }
         return View::create($category, Response::HTTP_OK, []);
     }
 
+    /**
+     * Count number of topics in one category by category_ID
+     * @param $id
+     * @return View
+     *
+     * @Route("api/countTopics/{id}", methods={"GET"})
+     *
+     * @SWG\Tag(name="Category")
+     * @SWG\Response(
+     *     response=200,
+     *     description="Returns number"
+     * )
+     * @SWG\Response(
+     *     response=404,
+     *     description="Returned when category no found"
+     * )
+     */
     public function countTopics($id)
     {
         $em = $this->getDoctrine()->getManager();
         $category = $em->getRepository('App\Entity\Categories')->find($id);
         $topics = $category->getTopics();
         if (empty($category)) {
-            return new View("Category can not be found", Response::HTTP_NOT_FOUND);
+            return new View(array("code" => 404 , "message" => "Category can not be found"), Response::HTTP_NOT_FOUND);
         }
         return View::create(count($topics), Response::HTTP_OK, []);
     }
